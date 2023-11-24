758316a36f6b24c2b6b4425db02b372795a14379
Maria Revelo <fer_nandita_8@hotmail.com>
2023-11-24 06:15:26 -0800

    Added country data and difficulty easy game structure

---
+++ data/countries_flags.js
@@ -0,0 +1,152 @@
+var countryData = [
+  {
+    "country": "Argentina",
+    "capital": "Buenos Aires",
+    "flag": "images/argentina.png"
+  },
+  {
+    "country": "Belgium",
+    "capital": "Brussels",
+    "flag": "images/belgium.png"
+  },
+  {
+    "country": "Brazil",
+    "capital": "Brasilia",
+    "flag": "images/brazil.png"
+  },
+  {
+    "country": "Canada",
+    "capital": "Ottawa",
+    "flag": "images/canada.png"
+  },
+  {
+    "country": "Chile",
+    "capital": "Santiago",
+    "flag": "images/chile.png"
+  },
+  {
+    "country": "China",
+    "capital": "Beijing",
+    "flag": "images/china.png"
+  },
+  {
+    "country": "Costa-rica",
+    "capital": "San Jose",
+    "flag": "images/costa-rica.png"
+  },
+  {
+    "country": "France",
+    "capital": "Paris",
+    "flag": "images/france.png"
+  },
+  {
+    "country": "Germany",
+    "capital": "Berlin",
+    "flag": "images/germany.png"
+  },
+  {
+    "country": "Guatemala",
+    "capital": "Guatemala City",
+    "flag": "images/guatemala.png"
+  },
+  {
+    "country": "India",
+    "capital": "New Delhi",
+    "flag": "images/india.png"
+  },
+  {
+    "country": "Indonesia",
+    "capital": "Jakarta",
+    "flag": "images/indonesia.png"
+  },
+  {
+    "country": "Italy",
+    "capital": "Rome",
+    "flag": "images/italy.png"
+  },
+  {
+    "country": "Japan",
+    "capital": "Tokyo",
+    "flag": "images/japan.png"
+  },
+  {
+    "country": "Malaysia",
+    "capital": "Kuala Lumpur",
+    "flag": "images/malaysia.png"
+  },
+  {
+    "country": "Mexico",
+    "capital": "Mexico City",
+    "flag": "images/mexico.png"
+  },
+  {
+    "country": "Netherlands",
+    "capital": "Amsterdam",
+    "flag": "images/netherlands.png"
+  },
+  {
+    "country": "Norway",
+    "capital": "Oslo",
+    "flag": "images/norway.png"
+  },
+  {
+    "country": "Panama",
+    "capital": "Panama City",
+    "flag": "images/panama.png"
+  },
+  {
+    "country": "Peru",
+    "capital": "Lima",
+    "flag": "images/peru.png"
+  },
+  {
+    "country": "Poland",
+    "capital": "Warsaw",
+    "flag": "images/poland.png"
+  },
+  {
+    "country": "Russia",
+    "capital": "Moscow",
+    "flag": "images/russia.png"
+  },
+  {
+    "country": "Singapore",
+    "capital": "Singapore",
+    "flag": "images/singapore.png"
+  },
+  {
+    "country": "South-korea",
+    "capital": "Seoul",
+    "flag": "images/south-korea.png"
+  },
+  {
+    "country": "Spain",
+    "capital": "Madrid",
+    "flag": "images/spain.png"
+  },
+  {
+    "country": "Sweden",
+    "capital": "Stockholm",
+    "flag": "images/sweden.png"
+  },
+  {
+    "country": "Thailand",
+    "capital": "Bangkok",
+    "flag": "images/thailand.png"
+  },
+  {
+    "country": "Uk",
+    "capital": "London",
+    "flag": "images/uk.png"
+  },
+  {
+    "country": "Usa",
+    "capital": "Washington, D.C.",
+    "flag": "images/usa.png"
+  },
+  {
+    "country": "Vietnam",
+    "capital": "Hanoi",
+    "flag": "images/vietnam.png"
+  }
+]
--- script/script.js
+++ script/script.js
@@ -27,23 +27,18 @@ title:"Matching game",
 isRunning :true,
 players:[],
 startGameButton: document.querySelector('.start-game'),
+playBtn: document.querySelector('.title-btn'),
 screenOne: document.querySelector('.screen1'),
 screenTwo: document.querySelector('.screen2'),
+screenThree: document.querySelector('.screen3'),
+screenFour: document.querySelector('.screen4'),
 joinButton: document.querySelector('.join-button'),
+btnEasy:document.querySelector('.btn-easy'),
+btnHard:document.querySelector('.btn-hard'),
 playerNameDisplay: document.querySelector('.name'),
 output: document.querySelector('.output'),
-toggleState(){
-     if (this.isRunning) {
-        this.screenOne.classList.add('hide');
-        this.screenTwo.classList.add('show');
-    } else {
-        this.screenOne.classList.remove('hide');
-        this.screenTwo.classList.remove('show');
-    }
-    // Toggle the running state of the game
-    this.isRunning = !this.isRunning;
+flagWrapper: document.querySelector('.flag-container'),
 
-},
 addPlayer:function(player){
 this.players.push(player)
 game.playerNameDisplay.value = "";
@@ -57,15 +52,31 @@ console.log(game.players)
 document.addEventListener('DOMContentLoaded', function () {
     let player;
     let newPlayerDiv;
+    let ShuffledOptions;
+    let WrongOptions;
+
+    function shuffleCountries(countryData){
+     return countryData[Math.floor(Math.random() * countryData.length)];
+    }
+
+    function WrongCountries(){
+        return countryData[Math.floor((Math.random())* countryData.length)];
+    }
+
+
 
     game.startGameButton.addEventListener('click', function(){
-    game.toggleState();
+        game.screenOne.style.display = 'none'
+        game.screenTwo.style.display = 'block'
     })
 
-
+    game.playerNameDisplay.addEventListener('keyup', function(){
+        
+    })
 
     game.joinButton.addEventListener('click', function(event){
         event.preventDefault();
+        console.log("hello")
 
         const inputValue = game.playerNameDisplay.value;
         
@@ -73,7 +84,7 @@ document.addEventListener('DOMContentLoaded', function () {
         // Create new player instance
         player = new Player(inputValue)
         game.addPlayer(player)
-        // game.output.textContent = inputValue;
+      
 
         game.players.forEach(player =>{
             newPlayerDiv = document.createElement('div')
@@ -81,7 +92,138 @@ document.addEventListener('DOMContentLoaded', function () {
 
             player.setElement(newPlayerDiv);
             console.log("player.setElemet",newPlayerDiv)
+            
+            let playerName = document.createElement('p')
+            playerName.classList.add('output-name')
+            playerName.textContent = inputValue;
+            newPlayerDiv.appendChild(playerName)
+
+            
+            
         })
+        game.output.appendChild(newPlayerDiv)
+
+        //If the game has atleast one player then we can start playing
+        if(game.players.length){
+            game.playBtn.classList.add('show')
+        }
 
     })
+
+    game.playBtn.addEventListener('click', function(e){
+        e.preventDefault()
+       game.screenTwo.style.display = 'none';
+       game.screenThree.style.display = 'block'
+  
+    })
+
+    function displayOptions(countryData){
+        let correctOption = shuffleCountries(countryData);
+        let options = new Set();
+        options.add(correctOption.country)
+
+        while(options.size <4){
+            let WrongOption = shuffleCountries(countryData);
+            if(WrongOption.country !== correctOption.country){
+                WrongOptions.add(WrongOption.country)
+            }
+        }
+        return [...options].sort(()=> Math.random()* - 0.5);
+    }
+    // function displayOptions(countryData){
+    //     ShuffledOptions = shuffleCountries(countryData)
+    //     WrongOptions = WrongCountries(countryData)
+    //     return[ShuffledOptions.country,WrongOptions.country]
+    // }
+    game.btnEasy.addEventListener('click',function(){
+        game.playBtn.textContent = "PLAY"
+        if(game.playBtn.textContent = "PLAY"){
+            game.playBtn.addEventListener('click',function(e){
+                e.preventDefault();
+                game.screenThree.style.display = 'none';
+                game.screenFour.style.display = 'block';
+
+                //Get a shuffled array of one correct and three wrong options
+                let options = displayOptions(countryData);
+
+                //Display the flag for the correct option
+                let correctOption = shuffleCountries(countryData)
+                const flagImage = document.createElement('img')
+                flagImage.src = country.flag; 
+                flagImage.alt = `Flag of ${country,country}`
+                game.flagWrapper.appendChild(flagImage)
+
+                //Display the options
+                options.forEach(option => {
+                const optionElement = document.createElement('li')
+                optionElement.textContent = option;
+                optionElement.addEventListener('click',function(){
+                //Check if option is correct
+                if(option == correctOption.country){
+                    console.log('correct')
+                } else{
+                    console.log('wrong!')
+                }
+                });
+                game.flagWrapper.appendChild(optionElement)
+
+
+                })
+
+                // ShuffledOptions = shuffleCountries(countryData)
+                // flagImage.src = ShuffledOptions.flag; 
+                // flagImage.alt = `Flag of ${ShuffledOptions.country}`
+               
+
+                ShuffledOptions = shuffleCountries(countryData)
+                country.textContent = ShuffledOptions.country; 
+                game.flagWrapper.appendChild(country)
+
+                const test = displayOptions(countryData);
+                const test1 = displayOptions(countryData);
+                const test2 = displayOptions(countryData);
+                let myArray =[test+test1+test2]
+                console.log("test",test,"test1",test1,"test2",test2,"my array",myArray)
+
+                // myArray.forEach((country,i) => {
+                // const country = document.createElement('p')
+                // country.textContent = myArray[i]
+                // game.flagWrapper.appendChild(country)
+                //   }
+                // )
+                
+
+                const wrong = document.createElement('p')
+                wrong.textContent = test
+                game.flagWrapper.appendChild(wrong)
+                
+            })
+           }
+    })
+  
+
+
+//   countryData.forEach(country => {
+//     const flagImage = document.createElement('img')
+//     flagImage.src = country.flag; 
+//     flagImage.alt = `Flag of ${country,country}`
+//     const OptionChoice = document.createElement('li')
+//     OptionChoice.classList.add('flag_country')
+//     OptionChoice.textContent = country.country
+//     game.flagWrapper.appendChild(OptionChoice);
+//     game.flagWrapper.appendChild(flagImage)
+    
+//   })
+
+
+// const shuffled = countryData[CorrectOptions]
+
+
+
+
+
+
+
+
+
 });
