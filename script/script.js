5282de07a59e57973d7a93090091339179ef27ba
Maria Revelo <fer_nandita_8@hotmail.com>
2023-12-05 19:56:07 -0800

    Added lives functionality

--- script/script.js
+++ script/script.js
@@ -9,7 +9,7 @@ class Player {
     constructor(name,element){
         this.name = name,
         this.score = 0,
-        this.lives = 5,
+        this.lives = 3,
         this.id = playerID++,
         this.element = element
     }
@@ -21,14 +21,60 @@ class Player {
         console.log("I used this TO SET THE ELEMENT!!!")
     }
 
+
+    increaseScore(amount = 1) {
+      this.score += amount;
+      // Update the DOM with the new score if necessary
+      const scoreElement = document.querySelector(`.screen4 #player-score-${this.id}`);
+        if (scoreElement) {
+          scoreElement.textContent = this.score; // Update the DOM with the new score
+        }else {
+          console.log("not found")
+        }
+  }
+    
+
+
+decreaseLives(amount = 1) {
+  this.lives -= amount;
+
+  // Update the heart images based on the current number of lives
+  for (let i = 1; i <= 3; i++) {
+      let heartImage = document.querySelector(`.screen4 #player-life-${i}-${this.id}`);
+      console.log(heartImage);
+      if (heartImage) {
+          if (i > this.lives) {
+              // Change the heart to an "empty" heart image to indicate the lost life
+              heartImage.src = "../images/gray-heart.png"; // Replace with your path to the empty heart image
+          } else {
+              heartImage.src = "../images/heart.png"; 
+          }
+      } else {
+          console.error(`Heart image not found for player ${this.id} life ${i}`);
+      }
+  }
+
+  if (this.lives <= 0) {
+      console.log("GAME OVER!!!"); 
+      game.flagWrapper.style.display = 'none';
+      const gameOverMessage = document.createElement('div');
+        gameOverMessage.textContent = "Game Over!";
+        console.log(gameOverMessage)
+        gameOverMessage.classList.add('game-over-message');
+        game.screenFour.appendChild(gameOverMessage);
+
+  }
+}
+
 }
 
 const game = {
 title:"Matching game",
 isRunning :true,
 players:[],
-currentPayer:"",
-currentDifficulty:"",
+currentPlayerIndex:0,
+isMultiplayer: false,
+currentDifficulty:'EASY', //Easy default
 startGameButton: document.querySelector('.start-game'),
 playBtn: document.querySelector('.title-btn'),
 screenOne: document.querySelector('.screen1'),
@@ -41,55 +87,100 @@ btnHard:document.querySelector('.btn-hard'),
 playerNameDisplay: document.querySelector('.name'),
 output: document.querySelector('.output'),
 flagWrapper: document.querySelector('.flag-container'),
+  
+// Example of adding a new player
+addPlayer: function(name) {
+  // Create a new player instance
+  let newPlayer = new Player(name);
+  // Add the new player to the array
+  this.players.push(newPlayer);
+  console.log(newPlayer);
+},
 
-
-//Method to add a player
-addPlayer:function(player){
-this.players.push(player)
-game.playerNameDisplay.value = "";
-console.log(game.players)
-},    
+nextPlayer: function(){
+  if(this.isMultiplayer){
+    this.currentPlayerIndex = (this.currentPlayerIndex + 1) % this.players.length;
+  }
+},
+ // Method to get the current player
+ getCurrentPlayer: function() {
+  return this.players[this.currentPlayerIndex];
+},
 
 // Method to update the display with player information
-  displayPlayers: function() {
-    // Clear the output container before appending new player divs
-  this.output.innerHTML = '';
-    this.players.forEach(player => {
-      // Create a new div for the player
-      let newPlayerDiv = document.createElement('div');
-      newPlayerDiv.classList.add('items-container');
-      
-      // Set the element on the player object
-      player.setElement(newPlayerDiv);
-      
-      // Create a paragraph for the player's name
-      let playerName = document.createElement('p');
-      playerName.classList.add('output-name');
-      playerName.textContent = player.name; // Should use player.name instead of inputValue
-      
-      // Append the player name to the player's div
-      newPlayerDiv.appendChild(playerName);
-      console.log('this whapenned once')
+displayPlayers: function(targetScreen) {
+  let outputContainer;
 
-      // Append the player's div to the game's output container
-      this.output.appendChild(newPlayerDiv);
-    });
-  },
+  // Determine which screen's output container to use
+  if (targetScreen === 'screen2') {
+      outputContainer = this.output; // Existing container for screen2
+  } else if (targetScreen === 'screen4') {
+      outputContainer = document.querySelector('.screen4 .player-info-container'); // Container for screen4
+  }
+
+  // Proceed only if a valid container is selected
+  if (outputContainer) {
+      outputContainer.innerHTML = ''; // Clear the container
+
+      this.players.forEach(player => {
+          let newPlayerDiv = document.createElement('div');
+          newPlayerDiv.classList.add('player-info');
+          console.log("this is happening",newPlayerDiv);
+
+          let playerName = document.createElement('p');
+          playerName.classList.add('player-name');
+          playerName.textContent = player.name;
+          console.log("this is happening again", playerName);
+
+          let playerScore = document.createElement('p');
+          playerScore.id = `player-score-${player.id}`;
+          playerScore.classList.add('player-score');
+          playerScore.textContent = player.score;
+          console.log("this is player score", playerScore);
+          console.log(player.score)
+
+          // Player.id is available and unique for each player
+          let playerLives = document.createElement('img');
+          playerLives.id = `player-life-1-${player.id}`;
+          playerLives.classList.add('player-lives');
+          playerLives.src = "../images/heart.png";
+
+          let playerLivesTwo = document.createElement('img');
+          playerLivesTwo.id = `player-life-2-${player.id}`;
+          playerLivesTwo.classList.add('player-lives');
+          playerLivesTwo.src = "../images/heart.png";
+
+          let playerLivesThree = document.createElement('img');
+          playerLivesThree.id = `player-life-3-${player.id}`;
+          playerLivesThree.classList.add('player-lives');
+          playerLivesThree.src = "../images/heart.png";
+          
+          newPlayerDiv.appendChild(playerName);
+          newPlayerDiv.appendChild(playerScore);
+          newPlayerDiv.appendChild(playerLives);
+          newPlayerDiv.appendChild(playerLivesTwo);
+          newPlayerDiv.appendChild(playerLivesThree);
+
+          outputContainer.appendChild(newPlayerDiv);
+      });
+  }
+},
 
  // Method to check if the game can start and to handle the play button click
  setupGameStart: function() {
     // If the game has at least one player, then we can start playing
     if (this.players.length) {
       this.playBtn.classList.add('show');
-      console.log("play button show")
+      console.log("play button show");
     }
 
 // Add click event listener to the play button
     this.playBtn.addEventListener('click', (e) => {
         e.preventDefault()
 
-          //Call the displayPlayers method to update the display
-        
+        // Update players on screen4
+        this.displayPlayers('screen4');
+
         //Switch the game to screen three
         this.screenTwo.style.display = 'none';
         this.screenThree.style.display = 'block';
@@ -126,6 +217,7 @@ console.log(game.players)
         console.log("button changes to PLAY")
         this.playBtn.style.color = 'yellow';
         this.currentDifficulty = difficulty; // Store the chosen difficulty
+        console.log(difficulty)
       },
 
 
@@ -183,6 +275,13 @@ let correctOption = options[Math.floor(Math.random() * options.length)];
 let correctCountryData = countryData.find(country => country.country === correctOption);
 
 this.displayFlag(correctCountryData);
+
+  // If the difficulty is HARD, add the country name and capital to the display
+  if (this.currentDifficulty === 'HARD') {
+    // Call a method to display additional information (country and capital)
+    this.displayAdditionalInfo(correctCountryData);
+}
+
 this.displayOptionButtons(options, correctCountryData);
 },
 
@@ -190,15 +289,20 @@ this.displayOptionButtons(options, correctCountryData);
 // Method to display option buttons
 displayOptionButtons: function(options, correctCountryData) {
 options.forEach(option => {
-const optionElement = document.createElement('li');
-optionElement.textContent = option;
-optionElement.classList.add('country-text');
-optionElement.addEventListener('click', () => {
-this.guessOutput(option, correctCountryData);
-});
-this.flagWrapper.appendChild(optionElement);
-});
+  const optionElement = document.createElement('li');
+  optionElement.textContent = option;
+  optionElement.classList.add('country-text');
+
+  optionElement.addEventListener('click', () => {
+  this.guessOutput(option, correctCountryData);
+  });
+  this.flagWrapper.appendChild(optionElement);
+  });
 },
+
+
+
+
 
 // Method to display the flag for the correct option
 displayFlag: function(correctCountryData) {
@@ -213,40 +317,33 @@ this.flagWrapper.appendChild(flagImage);
 // Method to handle guesses and output results
 guessOutput: function(option, correctCountryData) {
 this.clearOptions(); // Clear previous options and messages
+let currentPlayer = this.getCurrentPlayer();
 const guess = document.createElement('p');
 if (option === correctCountryData.country) {
     guess.style.color = 'green';
     guess.textContent = "CORRECT";
-    this.updatePlayerScore(1); 
+   currentPlayer.increaseScore(1); 
+    console.log(currentPlayer);
 } else {
     guess.style.color = 'red';
     guess.textContent = "WRONG";
-    this.updatePlayerLives(-1); // Subtract a life here
+    currentPlayer.decreaseLives(1); // Subtract a life here
+    console.log(currentPlayer);
 }
 this.flagWrapper.appendChild(guess);
 
 // Move to the next flag after a short delay
 setTimeout(() => {
     this.displayNextFlag(countryData);
-}, 2000);
+}, 500);
 },
 
 // Method to update the current player's score
 updatePlayerScore: function(score) {
-    let currentPlayer = this.players[this.players.length - 1]; 
-    currentPlayer.score += score;
-    // Update the score display logic here
+    let currentPlayer = this.getCurrentPlayer(); 
+    currentPlayer.increaseScore(score);
+  
 },
-
-// Method to update the current player's lives
-updatePlayerLives: function(lives) {
-    let currentPlayer = this.players[this.players.length - 1]; 
-    currentPlayer.lives += lives;
-    // Update the lives display logic here
-    if (currentPlayer.lives <= 0) {
-        // Handle game over logic here
-    }
-  },
 
 };
 
@@ -279,10 +376,11 @@ document.addEventListener('DOMContentLoaded', function (){
             const inputValue = game.playerNameDisplay.value.trim();
             if (inputValue){
                 // Create new player instance
-                player = new Player(inputValue)
-                game.addPlayer(player)
+                // player = new Player(inputValue)
+                game.addPlayer(inputValue)
                 //Display method to update UI
-                game.displayPlayers();
+                //Display players screen2
+                game.displayPlayers('screen2');
                 
                 //Clear input field
                 game.playerNameDisplay.value = '';
@@ -302,145 +400,3 @@ document.addEventListener('DOMContentLoaded', function (){
 
 });
 
-
-
-
-
-
-
-
-
-// game.joinButton.addEventListener('click', function(event){
-//     event.preventDefault();
-//     console.log("hello")
-
-//     const inputValue = game.playerNameDisplay.value;
-    
-
-//     // Create new player instance
-//     player = new Player(inputValue)
-//     game.addPlayer(player)
-  
-
-//     game.players.forEach(player =>{
-//         newPlayerDiv = document.createElement('div')
-//         newPlayerDiv.classList.add('items-container')
-
-//         player.setElement(newPlayerDiv);
-//         console.log("player.setElemet",newPlayerDiv)
-        
-//         let playerName = document.createElement('p')
-//         playerName.classList.add('output-name')
-//         playerName.textContent = inputValue;
-//         newPlayerDiv.appendChild(playerName)
-
-        
-        
-//     })
-//     game.output.appendChild(newPlayerDiv)
-
-//     //If the game has atleast one player then we can start playing
-//     if(game.players.length){
-//         game.playBtn.classList.add('show')
-//     }
-
-// })
-
-// game.playBtn.addEventListener('click', function(e){
-//     e.preventDefault()
-//    game.screenTwo.style.display = 'none';
-//    game.screenThree.style.display = 'block'
-
-// })
-
-// function displayOptions(countryData){
-//     let correctOption = shuffleCountries(countryData);
-//     console.log(correctOption);
-//     let options = new Set([correctOption.country]);
-//     // options.add(correctOption.country)
-
-//     //Add wrong options
-//     while(options.size <4){
-//         let wrongOption = shuffleCountries(countryData);
-//         if(wrongOption.country !== correctOption.country){
-//             options.add(wrongOption.country)
-//         }
-//     }
-
-//     //Convert the Set to anarray and shuffle it to randomize the order of options
-//     let optionsArray = [...options];
-//     shuffleArray(optionsArray);
-//     return optionsArray;
-   
-// }
-
-
-// function shuffleArray(array){
-// for (let i = array.length -1; i>0; i--){
-//     const j = Math.floor(Math.random()* (i+1));
-//     [array[i],array[j]] = [array[j],array[i]];
-// }
-// }
-
-// function guessOutput(option,correctCountryData){
-//     const guess = document.createElement('p')
-   
-//     if(option == correctCountryData.country){
-//         guess.style.color = 'green'
-//         guess.textContent = "CORRECT"
-
-//     }else {
-//         guess.style.color = 'red'
-//         guess.textContent = "WRONG"
-//     }
-//     game.flagWrapper.appendChild(guess);
-// }
-
-// game.btnEasy.addEventListener('click',function(){
-//     game.playBtn.textContent = "PLAY"
-//     if(game.playBtn.textContent = "PLAY"){
-//         game.playBtn.addEventListener('click',function(e){
-//             e.preventDefault();
-//             game.screenThree.style.display = 'none';
-//             game.screenFour.style.display = 'block';
-
-            
-//             // Clear previous options
-//             game.flagWrapper.innerHTML = '';
-
-//             //Get a shuffled array of one correct and three wrong options
-//             let options = displayOptions(countryData);
-
-//             // Find the correct option from the shuffled options
-//             let correctOption = options[Math.floor(Math.random() * options.length)];
-
-//               //Find the correct flag
-//               let correctCountryData = countryData.find(country => country.country === correctOption);
-
-
-           
-
-//             //Display the flag for the correct option
-//             const flagImage = document.createElement('img')
-
-          
-//             flagImage.src = correctCountryData.flag; 
-//             flagImage.alt = `Flag of ${correctCountryData.country}`
-//             flagImage.classList.add('flag')
-//             game.flagWrapper.appendChild(flagImage)
-
-//             //Display the options
-//             options.forEach(option => {
-//             const optionElement = document.createElement('li')
-//             optionElement.textContent = option;
-//             optionElement.classList.add('country-text')
-//             optionElement.addEventListener('click',function(){
-//             guessOutput(option,correctCountryData)
-    
-//             });
-//             game.flagWrapper.appendChild(optionElement)
-//             });
-            
-//         });
-//        }
-//}
